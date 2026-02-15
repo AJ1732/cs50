7179deaf74c8c91a04108bce3e3b7f7a59ae2935
AJ1732 <ejemeniboi@gmail.com>
2026-02-16 00:30:11 +0100

    chore: add type definitions

---
+++ week-4-memory/typedef/typedef.c
@@ -0,0 +1,17 @@
+struct student {
+    int id;
+    char name[20];
+    int age;
+};
+
+typedef struct student Student;
+
+typedef struct car 
+{
+  int year;
+  char model[10];
+  char plate[7];
+  int odometer;
+  double engine_size;
+} 
+Car;
